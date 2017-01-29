5d5dc66e8e283d17cdecfc5d836c66011fea5f64
Emil Buszylo <twj@email>
2017-01-29 13:55:04 +0100

    grid and menu test

---
+++ js/jq-menu.js
@@ -0,0 +1,35 @@
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 200) {
+        $('.navbar').addClass('sticky');
+    } else {
+        $('.navbar').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.navbar').hasClass('open-nav')) {
+        $('.navbar').removeClass('open-nav');
+    } else {
+        $('.navbar').addClass('open-nav');
+    }
+});
+
+$('.navbar li a').click(function() {
+    if ($('.navbar').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.navbar').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
